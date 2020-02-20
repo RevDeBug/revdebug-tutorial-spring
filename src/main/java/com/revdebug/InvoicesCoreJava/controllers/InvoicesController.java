ea7f908c1dd4310124e5a9a936588b63f94262db
Damian Tasevski <damian@revdebug.com>
2020-02-20 12:30:27 +0100

    Get products list from external server

--- src/main/java/com/revdebug/InvoicesCoreJava/controllers/InvoicesController.java
+++ src/main/java/com/revdebug/InvoicesCoreJava/controllers/InvoicesController.java
@@ -9,11 +9,14 @@ import javassist.bytecode.stackmap.BasicBlock;
 import org.hibernate.Session;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.server.ResponseStatusException;
 import org.xml.sax.SAXException;
 
@@ -68,11 +71,33 @@ public class InvoicesController {
 		ReconsileData reconsileData = new ReconsileData("InvoiceData.json");
 		JSONArray invoiceEntries = reconsileData.getInvoiceEntriesByInvoiceId(id);
 
+		RestTemplate restTemplate = new RestTemplate();
+		String resourceUrl = System.getenv("InvoicesCoreAddress") + "/Products/all";
+		String response = restTemplate.getForEntity(resourceUrl, String.class).getBody();
+
+		JSONParser parser = new JSONParser();
+		JSONArray productsArray = null;
+		try {
+			productsArray = (JSONArray) parser.parse(response);
+		}
+		catch (ParseException e) {
+			e.printStackTrace();
+		}
+
 		for (Object item : invoiceEntries) {
 			if (item instanceof JSONObject) {
 				int quantity = Integer.parseInt(((JSONObject) item).get("Quantity").toString());
 				int productId = Integer.parseInt(((JSONObject) item).get("ProducId").toString());
-				JSONObject product = reconsileData.getProductById(productId);
+
+				JSONObject product = null;
+				for (Object prod : productsArray) {
+					if (prod instanceof JSONObject) {
+						int prodId = Integer.parseInt(((JSONObject) prod).get("ProductId").toString());
+						if (prodId == productId) {
+							product = (JSONObject) prod;
+						}
+					}
+				}
 
 				String productName;
 				double unitPrice;
