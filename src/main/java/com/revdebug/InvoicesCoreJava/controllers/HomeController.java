c87ec0830cabbbaff18c7f517483ac158e9befdb
Hubert Kuffel <kuffel@tkomp.pl>
2020-06-08 11:42:58 +0200

    Endpoints for getval scenario

--- src/main/java/com/revdebug/InvoicesCoreJava/controllers/HomeController.java
+++ src/main/java/com/revdebug/InvoicesCoreJava/controllers/HomeController.java
@@ -2,6 +2,10 @@ package com.revdebug.InvoicesCoreJava.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.springframework.web.client.RestTemplate;
+import org.json.simple.JSONArray;
 
 @Controller
 public class HomeController {
@@ -9,6 +13,37 @@ public class HomeController {
 	@GetMapping("/")
 	public String home() {
 		return "home";
+	}
+
+	@GetMapping("/getval")
+	public String getval() {
+		try {
+			RestTemplate restTemplate = new RestTemplate();
+			String resourceUrl = "http://localhost:8083/processval";
+			restTemplate.getForEntity(resourceUrl, String.class).getBody();
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		return "val";
+	}
+
+	@GetMapping("/processval")
+	public String processval() {
+		try {
+			RestTemplate restTemplate = new RestTemplate();
+			String resourceUrl = "http://localhost:8084/calculateval";
+			restTemplate.getForEntity(resourceUrl, String.class).getBody();
+		}
+		catch (Exception e) {
+		}
+		return "val";
+	}
+
+	@GetMapping("/calculateval")
+	public String calculateval() {
+		int a = 6 / 0;
+		return "val";
 	}
 
 }
