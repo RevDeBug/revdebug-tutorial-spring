fde22d92b3a431069ee773f027bab5b9c439ea4e
michal lasek <michal.lasek@revdebug.com>
2021-05-19 13:51:39 +0200

    reworked into standalone app: -Replaced postgres with in-memory h2 -Removed dependence on having another instance running in .net and calling it for products

---
+++ src/main/java/com/revdebug/InvoicesCoreJava/models/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.revdebug.InvoicesCoreJava.models;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ProductRepository extends CrudRepository<Product, String> {
+
+}
